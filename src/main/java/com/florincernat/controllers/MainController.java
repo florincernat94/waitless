30792e3a6fbd2b1fb1a7ffe67db7fa49691de801
florincernat94 <florincernat94@gmail.com>
2018-04-19 00:06:45 +0300

    added some views

--- src/main/java/com/florincernat/controllers/MainController.java
+++ src/main/java/com/florincernat/controllers/MainController.java
@@ -4,10 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class BusinessController {
+public class MainController {
 	
-	/*@GetMapping("/register")
+	@GetMapping("/")
+	public String index(){
+		return "index";
+	}
+	
+	@GetMapping("/register")
 	public String greeting() {
 		return "pages/register";
-	}*/
+	}
 }
